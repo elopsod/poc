aecef8f377707006203dd88020cb71d85774d697
elopsod <elopsod@git.com>
2025-09-08 17:35:21 +0300

    fix

--- opensearch/keycloak/terraform/main.tf
+++ opensearch/keycloak/terraform/main.tf
@@ -38,19 +38,18 @@ resource "keycloak_saml_client" "app" {
     "saml.server.signature.keyinfo.ext" = false
   }
   valid_redirect_uris = [
-    "http://localhost:5601/_plugins/_security/saml/acs/",
-    "http://localhost:5601/_opendistro/_security/saml/acs/",
-    "http://127.0.0.1:8000/api/saml/callback",
-    "https://127.0.0.1:8443/api/saml/callback",
-    "http://127.0.0.1:5601/_opendistro/_security/saml/acs",
-    "http://localhost:5601/_opendistro/_security/saml/acs",
-    "http://127.0.0.1:5601/*",
-    "http://localhost:5601/*",
+    "http://opensearch-dashboards:5601/_plugins/_security/saml/acs/",
+    "http://opensearch-dashboards:5601/_opendistro/_security/saml/acs",
     "http://opensearch-dashboards:5601/*",
     "https://opensearch-dashboards:5601/*",
+    "http://127.0.0.1:8000/api/saml/callback",
   ]
 }
-
+resource "keycloak_saml_client_default_scopes" "this" {
+  realm_id       = keycloak_realm.realm.id
+  client_id      = keycloak_saml_client.app.id
+  default_scopes = []
+}
 # Group membership -> 'groups' attribute (aligns with roles_key: groups)
 resource "keycloak_generic_protocol_mapper" "groups" {
   realm_id        = keycloak_realm.realm.id
@@ -81,9 +80,10 @@ resource "keycloak_saml_user_property_protocol_mapper" "email" {
 resource "keycloak_user" "user" {
   realm_id = keycloak_realm.realm.id
   username = "user"
+  email_verified = true
   initial_password {
     value     = "user"
-    temporary = true
+    temporary = false
   }
   enabled    = true
   email      = "user@domain.com"
@@ -104,4 +104,14 @@ resource "keycloak_group_memberships" "group_members" {
   members = [
     keycloak_user.user.username
   ]
+}
+
+
+data "keycloak_realm_keys" "this" {
+  realm_id   = keycloak_realm.realm.id
+  algorithms = ["RS256"]
+}
+
+output "cert" {
+  value = data.keycloak_realm_keys.this.keys[0].certificate
 }
